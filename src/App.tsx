5176a18b5fca310f0d01d6e71e701c260d872a06
Mehran7kl <raisimehran7@gmail.com>
2024-01-10 22:03:49 +0330

    Working now

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,103 @@
-import { useState } from "react"
+import './App.css'
+import { useEffect, useState } from "react";
+import { Sudoku, idiv } from "./sudoku";
 
-function Cell({val=0}){
+
+
+
+function prepareSudoku(level: number):[Int32Array , Int32Array]{
+  const soduku = new Sudoku();
+  soduku.generateSudoku(level);  
+  const unsolvedTable = soduku.table.slice();
+
+  
+  
+  soduku.solve();
+  
+  const solvedTable = soduku.table;
+  return [unsolvedTable,solvedTable];
+}
+
+function Cell({index=0,initValue=0}){
+  
+  const [value,setValue] = useState(0);
+  
+  useEffect(()=>{
+    setValue(initValue);
+    
+  },[initValue]);
+
+  const col = index%9;
+  const row = idiv(index,9);
+  const srow = idiv(row,3);
+  const scol = idiv(col,3);
+  const sqIndex = srow*3 + scol;
+  
+  const isFixed = initValue > 0;
+
   return (
-  <div className=" box-border border-2 flex items-center justify-center">
-    <span>{val==0 ? null:val}</span>
+  <div onClick={()=>{
+    isFixed || setValue((value+1)%10);
+    
+  }}
+    className={ `${isFixed?" text-green-400":" text-black"} row-${row} col-${col} sq-${sqIndex} box-border border-2 flex items-center justify-center`}>
+    <span className='values'>{value>0 && value}</span>
   </div>
   )
 }
 
-function Square(){
-  return(
-  <div className=" border-2 border-black grid grid-cols-3">
-    {
-      new Array(9).fill(null).map((_v,i)=>{
-        return <Cell key={i}/>
-      })
-    }
-  </div>
-  )
-}
-
-function isValid(table:Int8Array){
-  const cols=new Int8Array(9);
-
-}
-function Board()
+function Board({level=3,solved=false})
 {
-  const [table,setTable] = useState(new Int8Array(81));
+  const [table,setTable] = useState(()=>prepareSudoku(level));
+  useEffect(()=>{
+    setTable(prepareSudoku(level));
+  },[level])
+  useEffect(()=>{
+    if(solved) setTable(t=>[t[1],t[1]]);
+  },[solved])
   
-  
-  const squraes :ReturnType< typeof Square>[]=new Array(9)
-  for(let i=0;i<9;i++) squraes[i] = <Square key={i}/>
+  const cells :ReturnType< typeof Cell>[]=new Array(81)
+  for(let i=0;i<81;i++) cells[i] = <Cell key={i} index={i} initValue={table[0][i]}/>
   return (
-  <div className="w-[90vmin] h-[90vmin] grid grid-cols-3">
-    {squraes}
+  <div className=" m-1 w-[90vmin] h-[90vmin] grid grid-cols-9 grid-rows-9">
+    
+    {cells}
+  
   </div>
+  )
+}
+function Switch({values,onChange,init=0}:{init:number,values:string[] ,onChange:(str:string)=>void})
+{
+  const [selected, setSelected] = useState(values[init])
+  return (
+    <div className=' rounded-md overflow-clip flex shadow'>
+      {
+        values.map(val=> <div key={val} onClick={()=>{
+          onChange(val);
+          setSelected(val);
+          
+        }} className={ (selected==val ? " bg-green-600 " : "bg-slate-300")+ ' flex-auto shadow-lg transition-colors p-3  '}>{val}</div>)
+      }
+    </div>
   )
 }
 export default function App() {
+  const [levelName,setLevelName] = useState('Amator');
+  const [solved,setSolved] = useState(false);
+  const level = ({
+    Amator:0.2,
+    Expert:0.5,
+    Master:0.7,
+  })[levelName];
+  
+  
   return (
-    <div className=" flex justify-center" >
-      <Board />
+    <div className=" flex items-center flex-col" >
+      <Board solved={solved} level={level}/>
+      <div className=' gap-2 justify-center self-stretch flex '>
+        <button className=' rounded-md shadow-lg  p-3 bg-slate-300' onClick={()=>setSolved(true)}>Solve</button>
+        <Switch init={1} values={["Amator","Expert","Master"]} onChange={str=>{setSolved(false); setLevelName(str)}}/>
+      </div>
     </div>
   )
 }
--- src/sudoku.ts
+++ src/sudoku.ts
@@ -6,7 +6,7 @@
  * @description performs integer division
  * @example idiv(8,3) -> 2
  */
-function idiv(a:number,b:number)
+export function idiv(a:number,b:number)
 {
     return ~~(a/b)
 }
@@ -19,9 +19,9 @@ function randomInt(start:number, end: number)
 export class Sudoku{
     
     // An array which holds the values 
-    private readonly table = new Int32Array(81);
+    readonly table = new Int32Array(81);
     // An array that keeps which cells are prefilled
-    private readonly fixedCells:boolean[] = new Array(81);
+    readonly fixedCells:boolean[] = new Array(81);
     //some arrays with 9 integers per each and every integer is a bitset that holds 9 boolean values
     // that shows which col/row/square has already has that number
     // for example: 
@@ -83,45 +83,54 @@ export class Sudoku{
     }
 
     /**
-     * @description generates a sudoku to be soved ny user
+     * @description generates a sudoku to be soved ny user; 9! possible sudokus.
      * @param level level of difcualty; an integer in range [2,9] easy-> hard
      */
-    public generateSudoku(level:number = 2)
+    public generateSudoku(level:number )
     {
-        
-        for(let i=randomInt(2,level);i<81;i+=randomInt(2,level))
+        const temp= new Sudoku();
+        const arr=[1,2,3,4,5,6,7,8,9];
+        arr.forEach((v,i,arr)=>{
+            
+            const otherIndex = randomInt(1,10);
+            arr[i]=arr[otherIndex];
+            arr[otherIndex]=v;
+        })
+        for(let i=0,j=0;i<81;i+=10,j++)
         {
-            for(let v=1;v<10;v++)
-            {
-                try{
-                    this.putFixedCell(i,v);
-                    break;
-                }catch(e){
-                    continue;
-                }
-                
-            }
-
+            temp.putFixedCell(i,arr[j]);
+        }
+        temp.solve();
+        for(let i=0;i<81;i++){
+            if(Math.random()>level) this.putFixedCell(i,temp.table[i])
         }
     }
     /**
-     * @description puts values into table regarding they are fixed and can't change.
+     * @description puts values into table regarding they are fixed (if non-zero) and can't change.
      * @param args 81 numbers that represent sudoku values; non-zero values are fixed.
      */
     public putFixedCells(...args: number[])
     {
         args.forEach((value,index)=>{
-            this.putFixedCell(index, value);
+            if(!this.putFixedCell(index, value)) return false;
         })
+        return true;
     }
-
     public putFixedCell(index: number, value: number)
     {
-        if(!this.putValue(index, value)) throw "duplicate value ";
+        if(!this.putValue(index, value)) return false;
         if(value!=0) this.fixedCells[index]=true;
+        return true;
     }
+    /**
+     * @description solves the sudoku
+     * @returns wether could solve or not
+     */
     public solve()
     {
+        /*
+        I'm using an algoritm known as backtracking which is a depth-first broute-force search algoritm.
+        */
         let firstCell = 0;
         let i=0;
         while(this.fixedCells[firstCell]) firstCell++;
@@ -145,27 +154,30 @@ export class Sudoku{
         const val=()=> this.table[i];
         
         while(i<81){
+            
             let newVal;
             // increment cuurent value
             for(newVal=val()+1;newVal<10;newVal++)
             {
                 //if the new value is valid go for next cell; otherwise keep incrementing
                 if(this.putValue(i,newVal)) break;
-                
+
             }
-            //if no value was valid then this state is not valid erace the cell amd go back
+            //if no value was valid then this state is not valid erase the cell and go back
             if(newVal == 10)
             {
                 //if this is first empty cell, so we can't backtrack futhermore
                 if(i == firstCell && val()== 9) return false; 
-                // if this 
+                
                 this.putValue(i,0);
                 back();
+                
                 continue;
             }
             next();
         }
-
+        
+        return true;
     }
 
 }
